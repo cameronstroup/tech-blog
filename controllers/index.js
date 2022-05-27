dc296da79889c7f936f470b09e6d117cd844417b
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-05-27 14:43:41 -0500

    server works

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+// const homeRoutes = require("./homeRoutes");
+
+// router.use("/", homeRoutes);
+// z
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS techieBlog_db;
+CREATE DATABASE techieBlog_db
